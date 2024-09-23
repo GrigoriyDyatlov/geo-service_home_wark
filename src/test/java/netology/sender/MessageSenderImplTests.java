84ed7092b6f9e8104e85b967c31e0f6c6ff095ab
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-09-23 20:28:50 +0300

    home work 2

--- src/test/java/netology/sender/MessageSenderImplTests.java
+++ src/test/java/netology/sender/MessageSenderImplTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
 import ru.netology.sender.MessageSenderImpl;
 
@@ -19,8 +23,11 @@ LocalizationServiceImpl localizationService;
     @Test
     public void sendTestWithMockitoRussiaIp() {
         String ip = "172.0.32.11";
-        GeoServiceImpl geoServiceImpl = Mockito.spy(new GeoServiceImpl());
-        LocalizationServiceImpl localizationServiceImpl = Mockito.spy(new LocalizationServiceImpl());
+        GeoService geoServiceImpl = Mockito.mock(GeoServiceImpl.class);
+Mockito.when(geoServiceImpl.byIp(ip)).thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
+LocalizationService localizationServiceImpl = Mockito.mock(LocalizationServiceImpl.class);
+Mockito.when(localizationServiceImpl.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+
 
         Map<String, String> header = Map.of(
                 MessageSenderImpl.IP_ADDRESS_HEADER, ip);
